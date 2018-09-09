b538c65b56da81c563efd462650e23e419ae7722
Cara Morelli <caramorelli7@gmail.com>
2018-09-09 14:32:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    var children = rankedLists[i].children
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
+  }
+}
+
+function deepestChild() {
+  var node = document.getElementById('grand-node')
+  var nextNode = node.children[0]
+
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+}
